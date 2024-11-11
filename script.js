746c03d9e9b0d043f0e5ddcc39f283756fc0ca8a
Van Minh <kenmoster19@gmail.com>
2024-11-11 22:01:42 +0700

    Chua them duoc api vao mang

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+const btnReload = document.querySelector('.reload-button');
+
+let allQuotes = [];
+fetch('https://api.api-ninjas.com/v1/quotes') // Thay bằng URL của API bạn muốn gọi
+  .then((response) => response.json()) // Chuyển đổi dữ liệu JSON trả về từ API
+  .then((data) => {
+    allQuotes = data; // Lưu dữ liệu từ API vào mảng allQuotes
+  })
+  .catch((error) => console.error('Error fetching data:', error));
+
+btnReload.addEventListener('click', function () {
+  const randomIndex = Math.floor(Math.random() * allQuotes.length);
+  document.querySelector('.text').textContent = allQuotes[randomIndex];
+});
